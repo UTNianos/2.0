263ce41a3fae082e9e0ca73b85c25560ce2aaab5
Javier <javier.rotelli@gmail.com>
2016-03-08 12:58:21 -0300

    agrego helpers para construccion de forms

--- config/app.php
+++ config/app.php
@@ -159,6 +159,8 @@ return [
         //ide helper
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -204,6 +206,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
 
     ],
 
