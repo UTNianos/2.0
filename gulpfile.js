d51dded7e7534d94543bd69ed099c046db7cfeef
Julian Selser <julianselser@yahoo.com.ar>
2016-03-21 03:25:06 -0300

    Gulp clean ya no borra assets/js

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,10 @@ elixir(function (mix) {
 
 gulp.task('clean', function () {
     return del([
+        'public/build/**/*',
         'public/assets/**/*',
-        'public/build/**/*'
+        '!public/assets',
+        '!public/assets/js',
+        '!public/assets/js/.gitkeep',
     ]);
 });
